5799d84c77d18b1ff067f0afe48e08f199a1d4ee
Spencer Lee Meredith <16077691+SLM-Dev@users.noreply.github.com>
2021-06-10 00:08:59 +0000

    ✨ add feature to only scroll up and down, sidescroll ♻️ fixed typos added box interactivity

--- script.js
+++ script.js
@@ -8,17 +8,19 @@ checkBoxes()
 function checkBoxes() {
     const triggerBottom = window.innerHeight / 5 * 4
 
-    boxes.forEach(box =>) {
+    boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top
     
-    
+    // Once it is true, soon top next box trigger bottom point its going to come in
         if(boxTop < triggerBottom) {
             box.classList.add('show')
         } else {
+
+            // if not true it will remove the class, it will remove the blocks
             box.classList.remove('show')
         }
 
-
+    })
 }
 
 
